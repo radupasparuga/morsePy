22db749cd76702a67b6c1df90911a5aac8c76c53
radupasparuga <radupasparuga25@gmail.com>
2019-04-10 20:40:40 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='morsePy',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/radupasparuga/morsePy',
+    license='MIT',
+    author='Radu Pasparuga',
+    author_email='radupasparuga25@gmail.com',
+    description='A morse code translator in python'
+)
